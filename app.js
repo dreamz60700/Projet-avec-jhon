d94a22d12a61b3c9027a0944f8315997e168e519
j314h <johnhaimez314@gmail.com>
2020-03-13 12:20:22 +0100

    init fonction recherche

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
+//dom du texte des cadre
 let nom = document.querySelector('.nom')
 let message = document.querySelector('#texte1')
+//dom btn loupe du formulaire
+let btnRecherche = document.querySelector('.loupe')
 
-let jsonU = fetch("utilisateurs.json")
+btnRecherche.onclick = () => {
+//dom input recherche
+let recherche = document.getElementById('inrecherche').value
 
-jsonU.then((res) => {
-    res.json().then((res) => {
+    if (recherche == "") {
 
-        nom.textContent = res.utilisateur[0].prenom + " " + res.utilisateur[0].name
+        console.log("veuillez renseigner le champs")
+        
+    }else{
+        let jsonU = fetch("utilisateurs.json")
+        //recherche dans le json uutilisateur
+        jsonU.then((res) => {
+            res.json().then((res) => {
 
-        console.log(nom.textContent)
-    })
-})
+                res.utilisateur.forEach(utilisateur => {
 
+                    if (recherche == utilisateur.name){
+                        nom.textContent = utilisateur.prenom + " " + utilisateur.name
+                        message.textContent = utilisateur.message
+                    }else{
+                        console.log('erreur')
+                    }
+
+                    console.log(utilisateur.name)
+                    console.log(utilisateur.prenom)
+                    console.log(utilisateur.message)
+                });
+
+                console.log(res.utilisateur)
+        
+            })
+        })
+    
+    }
+
+}
+
+
+/*res.forEach(utilisateur => {
+                    console.log(utilisateur)
+                })
+                
+                nom.textContent = res.utilisateur[0].prenom + " " + res.utilisateur[0].name*/
